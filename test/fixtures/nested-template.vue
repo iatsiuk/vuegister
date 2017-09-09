ca108072c6267ef0867bc2cc64efaa817e1e01d2
Андрей Макарычев <makarychev@reg.ru>
2017-09-09 21:11:01 +0400

    update test

---
+++ test/fixtures/nested-template.vue
@@ -0,0 +1,25 @@
+<!-- basic.vue -->
+
+<template>
+  <h2 class="red">{{msg}}</h2>
+  <template v-if="true">
+    <h3>Header3</h3>
+  </template>
+</template>
+
+<script>
+  module.exports = {
+    // test data
+    data () {
+      return {
+        msg: 'Hello'
+      }
+    }
+  }
+</script>
+
+<style>
+  comp-a h2 {
+    color: #f00;
+  }
+</style>
